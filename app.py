fcd4ccaeb7c96202a89e64d3c27a329dcf56260c
HunkattenMiv <hilgermathias@gmail.com>
2026-06-04 16:09:03 +0200

    Hindbærbrus

--- app.py
+++ app.py
@@ -17,22 +17,79 @@ def home():
     }
 
     return render_template("index.html", project=project)
+
+@app.route("/signup", methods = ['GET','POST'])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        salt = "497hg4"
+        ##ADD SALT TO HASHED PASSWORD
+        role = request.form.get("role")
+        ##Beboer
+        beboer_id = None
+        if role == "beboer":
+            name = request.form.get("name")
+            room = request.form.get("room_number")
+            result = conn.execute(text("""
+                        INSERT INTO beboer (name, room_number, madklub_taken, amount_cleanings, cleanings_done)
+                              VALUES (:name, :room_number, :madklub_taken, :amount_cleanings, :cleanings_done)
+                              """),
+                              {
+                                  "name": name,
+                                  "room_number": room,
+                                  "madklub_taken": False,
+                                  "amount_cleanings": 2,
+                                  "cleanings_done": 0
+                              })
+            print("Beboer created!!")
+            beboer_id = result.scalar()
+            print("Creating a new beboer with ID: {beboer_id}")
+            conn.commit()
+
+
+        conn.execute(text("""
+                      INSERT INTO user_table (beboer_id, role, username, password)
+                      VALUES (:beboer_id, :role, :username, :password)
+                      """),
+                      {
+                          "beboer_id": beboer_id,
+                          "role": role,
+                          "username": username,
+                          "password": password
+                      })
+        conn.commit()
+        return "User Created"
+    return render_template("signup.html")
+
 @app.route("/foodclub/", methods = ['GET', 'POST'])
 def foodclub():
+    result = conn.execute(text("""
+                    SELECT * FROM madklub;
+                      """))
+    result = result.fetchall()
+
+    food = {
+        "name": result[0][2],
+        "description": "A platform to organize and manage your food club activities.",
+        "features": [
+            "Event scheduling",
+            "Member management"
+        ],
+    }
+
+    return render_template("foodclub.html", food = food)
+
+@app.route("/foodclub/add", methods = ['GET', 'POST'])
+def add_foodclub():
+
     if request.method == "POST":
         menu = request.form.get("menu") 
         close_at = request.form.get("close_at") 
         start_at = request.form.get("start_at") 
-        vege = request.form.get("vege")
-        if vege == "on":
-            vege = True
-        else:
-            vege = False
-        vegan = request.form.get("vegan")
-        if vegan == "on":
-            vegan = True
-        else:
-            vegan = False
+        vege = request.form.get("vege") == "on"
+        vegan = request.form.get("vegan") == "on"
+
         print("Adding to database")
         conn.execute(text("""
                           INSERT INTO madklub (id, beboer_id, menu, close_at, start_at, vege, vegan, price, picture)
@@ -51,16 +108,7 @@ def foodclub():
                           }
                           )
         conn.commit()
-    food = {
-        "name": "Food Club",
-        "description": "A platform to organize and manage your food club activities.",
-        "features": [
-            "Event scheduling",
-            "Member management"
-        ],
-    }
-
-    return render_template("foodclub.html", food = food)
+    return render_template("foodclubadd.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
